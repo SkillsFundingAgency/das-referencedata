8834442f2ce6fe1a93fc6d7155ec6d708980d9a4
Robin O'Connor <github@bytepointsoftware.com>
2016-12-14 16:24:58 +0000

    Initial solution skeleton

---
+++ src/SFA.DAS.ReferenceData.Api/Controllers/OrganisationController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace SFA.DAS.ReferenceData.Api.Controllers
+{
+    public class OrganisationController : Controller
+    {
+        public string Index()
+        {
+            return "New API";
+        }
+    }
+}
---
+++ src/SFA.DAS.ReferenceData.Organisations.Database/Tables/PublicOrganisation.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[PublicOrganisation]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(250) NOT NULL
+)
