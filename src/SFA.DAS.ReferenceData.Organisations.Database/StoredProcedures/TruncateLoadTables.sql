5318920f9ea04d520e23ac9b709fd6fda9c34382
Christopher Foster <Christopher Foster>
2016-12-15 15:26:15 +0000

    Adds load table truncation proc

---
+++ src/SFA.DAS.ReferenceData.Organisations.Database/StoredProcedures/TruncateLoadTables.sql
@@ -0,0 +1,21 @@
+﻿CREATE PROCEDURE [CharityImport].[TruncateLoadTables]
+AS
+
+TRUNCATE TABLE [CharityImport].extract_acct_submit
+TRUNCATE TABLE [CharityImport].extract_aoo_ref
+TRUNCATE TABLE [CharityImport].extract_ar_submit
+TRUNCATE TABLE [CharityImport].extract_charity
+TRUNCATE TABLE [CharityImport].extract_charity_aoo
+TRUNCATE TABLE [CharityImport].extract_class
+TRUNCATE TABLE [CharityImport].extract_class_ref
+TRUNCATE TABLE [CharityImport].extract_financial
+TRUNCATE TABLE [CharityImport].extract_main_charity
+TRUNCATE TABLE [CharityImport].extract_name
+TRUNCATE TABLE [CharityImport].extract_objects
+TRUNCATE TABLE [CharityImport].extract_overseas_expend
+TRUNCATE TABLE [CharityImport].extract_partb
+TRUNCATE TABLE [CharityImport].extract_registration
+TRUNCATE TABLE [CharityImport].extract_remove_ref
+TRUNCATE TABLE [CharityImport].extract_trustee
+
+
