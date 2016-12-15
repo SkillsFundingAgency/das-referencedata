a3011d21e98ca00f0ea2d0ea0f3f1d1b3bdf1720
Robin O'Connor <GitHub@bytepointsoftware.com>
2016-12-15 15:09:49 +0000

    tests to make the build work

---
+++ src/SFA.DAS.ReferenceData.Application.UnitTests/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SFA.DAS.ReferenceData.Application.UnitTests
+{
+    public class Class1
+    {
+        [SetUp]
+        public void Arrange()
+        {
+
+        }
+
+        [Test]
+        public void Then()
+        {
+            //Arange
+
+
+            //Act
+
+
+            //Assert
+            Assert.IsFalse(false);
+        }
+    }
+}
