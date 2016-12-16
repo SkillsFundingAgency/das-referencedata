20a9b80a61145ce221381ddd423ce9117841bb2d
Christopher Foster <Christopher Foster>
2016-12-16 15:08:59 +0000

    Adds unit test proj

---
+++ src/SFA.DAS.ReferenceData.CharityImport.WebJob.UnitTests/CharityImporterTests/WhenIImportCharityData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SFA.DAS.ReferenceData.CharityImport.WebJob.UnitTests.CharityImporterTests
+{
+    [TestClass]
+    public class WhenIImportCharityData
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
