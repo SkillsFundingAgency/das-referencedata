dfcf4a17ff638d657b26ff40926494cc1c424d2f
Christopher Foster <Christopher Foster>
2016-12-21 11:59:22 +0000

    Adds Api Client project

---
+++ src/SFA.DAS.ReferenceData.Api.Client/IReferenceDataApiClient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.ReferenceData.Api.Client
+{
+    public interface IReferenceDataApiClient
+    {
+        Task<Dto.Charity> GetCharity(string regisrationNumber);
+    }
+}
---
+++ src/SFA.DAS.ReferenceData.Api.Client/ReferenceDataApiClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SFA.DAS.ReferenceData.Api.Client
+{
+    public class ReferenceDataApiClient :IReferenceDataApiClient
+    {
+        private readonly ReferenceDataApiConfiguration _configuration;
+        private readonly SecureHttpClient _httpClient;
+
+        public ReferenceDataApiClient(ReferenceDataApiConfiguration configuration)
+        {
+            _configuration = configuration;
+            _httpClient = new SecureHttpClient(configuration);
+        }
+
+        internal ReferenceDataApiClient(ReferenceDataApiConfiguration configuration, SecureHttpClient httpClient)
+        {
+            _configuration = configuration;
+            _httpClient = httpClient;
+        }
+
+        public async Task<Dto.Charity> GetCharity(string regisrationNumber)
+        {
+            var baseUrl = _configuration.ApiBaseUrl.EndsWith("/")
+                ? _configuration.ApiBaseUrl
+                : _configuration.ApiBaseUrl + "/";
+
+            var url = $"{baseUrl}charity/{regisrationNumber}";
+
+            var json = await _httpClient.GetAsync(url);
+
+            return JsonConvert.DeserializeObject<Dto.Charity>(json);
+        }
+    }
+}
