4b08885adab1b46ba1c179c160343e79b72efabb
Christopher Foster <Christopher Foster>
2016-12-28 14:33:45 +0000

    Adds a flag to denote a Removed charity Trims extra white space

--- src/SFA.DAS.ReferenceData.Organisations.Database/StoredProcedures/GetCharityByRegistrationNumber.sql
+++ src/SFA.DAS.ReferenceData.Organisations.Database/StoredProcedures/GetCharityByRegistrationNumber.sql
@@ -6,13 +6,14 @@ AS
 
 	select
 	charity.regno as 'RegistrationNumber',
-	charity.name as 'Name',
-	charity.add1 as 'Address1',
-	charity.add2 as 'Address2',
-	charity.add3 as 'Address3',
-	charity.add4 as 'Address4',
-	charity.add5 as 'Address5',
-	registration.regdate as 'RegistrationDate'
+	rtrim(charity.name) as 'Name',
+	rtrim(charity.add1) as 'Address1',
+	rtrim(charity.add2) as 'Address2',
+	rtrim(charity.add3) as 'Address3',
+	rtrim(charity.add4) as 'Address4',
+	rtrim(charity.add5) as 'Address5',
+	registration.regdate as 'RegistrationDate',
+	CAST (CASE WHEN charity.orgtype = 'RM' THEN 1 ELSE 0 END AS BIT) 'IsRemoved'
 	from
 	[CharityData].[charity] charity
 	inner join [CharityData].[registration] registration on registration.regno = charity.regno and registration.subno = charity.subno
@@ -20,5 +21,3 @@ AS
 	charity.regno = @RegistrationNumber
 	and charity.subno = 0
 	
-
-
