e78c47203bf074c3fe7989db683fac69bf8c7f23
Christopher Foster <Christopher Foster>
2016-12-29 09:31:04 +0000

    Adds Authorize attribute to api endpoints

--- src/SFA.DAS.ReferenceData.Api/Controllers/OrganisationController.cs
+++ src/SFA.DAS.ReferenceData.Api/Controllers/OrganisationController.cs
@@ -18,6 +18,7 @@ namespace SFA.DAS.ReferenceData.Api.Controllers
 
         [Route("publicsectorbodies", Name = "Public Sector")]
         [HttpGet]
+        [Authorize]
         public async Task<IHttpActionResult> GetPublicSectorOrganisations(string searchTerm = "", int pageSize = 1000, int pageNumber = 1)
         {
             var response = await _mediator.SendAsync(new FindPublicSectorOrgainsationQuery
@@ -32,6 +33,7 @@ namespace SFA.DAS.ReferenceData.Api.Controllers
 
         [Route("charities/{registrationNumber}", Name = "Charity")]
         [HttpGet]
+        [Authorize]
         public async Task<IHttpActionResult> GetCharity(int registrationNumber)
         {
             var query = new GetCharityByRegistrationNumberQuery
