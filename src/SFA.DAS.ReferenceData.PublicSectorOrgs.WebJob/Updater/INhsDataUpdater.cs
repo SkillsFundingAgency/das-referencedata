035d81d870716f57d117f557dd5413dda3ec8e5e
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-03-24 14:02:13 +0000

    Split out nhs update into seperate class
    Seperated out the nhs updater into a seperate class and changed the
    logic so that it is now reading from a list of URLs and downloading and
    unzipping the data.

---
+++ src/SFA.DAS.ReferenceData.PublicSectorOrgs.WebJob/Updater/INhsDataUpdater.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using SFA.DAS.ReferenceData.Domain.Models;
+
+namespace SFA.DAS.ReferenceData.PublicSectorOrgs.WebJob.Updater
+{
+    public interface INhsDataUpdater
+    {
+        Task<PublicSectorOrganisationLookUp> GetData();
+    }
+}
