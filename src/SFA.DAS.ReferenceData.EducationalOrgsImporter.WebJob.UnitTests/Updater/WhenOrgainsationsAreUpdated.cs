4823cbe6efadfed4171eb4e7d3b91599a6202a59
Robin O'Connor <github@bytepointsoftware.com>
2017-06-09 14:13:41 +0100

    Added test project for educational organisation updater

---
+++ src/SFA.DAS.ReferenceData.EducationalOrgsImporter.WebJob.UnitTests/Updater/WhenOrgainsationsAreUpdated.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace SFA.DAS.ReferenceData.EducationalOrgsImporter.WebJob.UnitTests.Updater
+{
+    class WhenOrgainsationsAreUpdated
+    {
+        [SetUp]
+        public void Arrange()
+        {
+            
+        }
+    }
+}
