313c08b1e168d61b6b83afe3ef08958dfcfdc76c
Robin O'Connor <github@bytepointsoftware.com>
2017-06-09 16:24:16 +0100

    New tests for the updater

--- src/SFA.DAS.ReferenceData.EducationalOrgsImporter.WebJob.UnitTests/Updater/WhenOrgainsationsAreUpdated.cs
+++ src/SFA.DAS.ReferenceData.EducationalOrgsImporter.WebJob.UnitTests/Updater/WhenOrgainsationsAreUpdated.cs
@@ -1,13 +1,135 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using Moq;
+using NLog;
+using NUnit.Framework;
+using SFA.DAS.ReferenceData.Domain.Interfaces.Services;
+using SFA.DAS.ReferenceData.Domain.Models.Education;
+using SFA.DAS.ReferenceData.EducationOrgsImporter.WebJob.Azure;
+using SFA.DAS.ReferenceData.EducationOrgsImporter.WebJob.Serializer;
+using SFA.DAS.ReferenceData.EducationOrgsImporter.WebJob.Updater;
 
 namespace SFA.DAS.ReferenceData.EducationalOrgsImporter.WebJob.UnitTests.Updater
 {
-    class WhenOrgainsationsAreUpdated
+    public class WhenOrgainsationsAreUpdated
     {
+        private EducationalOrgsUpdater _updater;
+        private Mock<IEdubaseService> _edubaseService;
+        private Mock<IEducationalOrgainsationSerialiser> _serialiser;
+        private Mock<IAzureStorageUploader> _uploader;
+        private Mock<ILogger> _logger;
+
+
         [SetUp]
         public void Arrange()
         {
-            
+            _edubaseService = new Mock<IEdubaseService>();
+            _serialiser = new Mock<IEducationalOrgainsationSerialiser>();
+            _uploader = new Mock<IAzureStorageUploader>();
+            _logger = new Mock<ILogger>();
+
+            _updater = new EducationalOrgsUpdater(
+                _edubaseService.Object,
+                _serialiser.Object,
+                _uploader.Object,
+                _logger.Object);
+        }
+
+        [Test]
+        public async Task ThenShouldUploadCollectedOrgansiations()
+        {
+            //Arrange
+            var organisations = new List<EducationOrganisation> {new EducationOrganisation()};
+            var jsonData = new byte[10];
+
+            _edubaseService.Setup(x => x.GetOrganisations())
+                           .ReturnsAsync(organisations);
+
+            _serialiser.Setup(x => x.SerialiseToJson(It.IsAny<IEnumerable<EducationOrganisation>>()))
+                       .Returns(jsonData);
+
+            //Act
+            await _updater.RunUpdate();
+
+            //Assert
+            _edubaseService.Verify(x => x.GetOrganisations(), Times.Once);
+            _serialiser.Verify(x => x.SerialiseToJson(organisations), Times.Once);
+            _uploader.Verify(x => x.UploadDataToStorage(jsonData), Times.Once);
+        }
+
+        [Test]
+        public async Task ThenShouldNotUploadOrgainsationsIfNoneFound()
+        {
+            //Arrange
+            _edubaseService.Setup(x => x.GetOrganisations())
+                           .ReturnsAsync(new List<EducationOrganisation>());
+
+            //Act
+            await _updater.RunUpdate();
+
+            //Assert
+            _edubaseService.Verify(x => x.GetOrganisations(), Times.Once);
+            _serialiser.Verify(x => x.SerialiseToJson(It.IsAny<IEnumerable<EducationOrganisation>>()), Times.Never);
+            _uploader.Verify(x => x.UploadDataToStorage(It.IsAny<byte[]>()), Times.Never);
+        }
+
+        [Test]
+        public async Task ThenShouldLogErrorIfEdubaseThrowsOne()
+        {
+            //Arrange
+            var exception = new WebException();
+
+            _edubaseService.Setup(x => x.GetOrganisations()).Throws(exception);
+
+            //Act
+            await _updater.RunUpdate();
+
+            //Assert
+            _logger.Verify(x => x.Error(exception, It.IsAny<string>()), Times.Once);
+            _serialiser.Verify(x => x.SerialiseToJson(It.IsAny<IEnumerable<EducationOrganisation>>()), Times.Never);
+        }
+
+        [Test]
+        public async Task ThenShouldLogErrorIfSerialiserThrowsOne()
+        {
+            //Arrange
+            var exception = new SerializationException();
+          
+
+            _edubaseService.Setup(x => x.GetOrganisations())
+                           .ReturnsAsync(new List<EducationOrganisation> { new EducationOrganisation() });
+
+            _serialiser.Setup(x => x.SerialiseToJson(It.IsAny<IEnumerable<EducationOrganisation>>()))
+                       .Throws(exception);
+
+            //Act
+            await _updater.RunUpdate();
+
+            //Assert
+            _logger.Verify(x => x.Error(exception, It.IsAny<string>()), Times.Once);
+        }
+
+        [Test]
+        public async Task ThenShouldLogErrorIfAzureUploaderThrowsOne()
+        {
+            //Arrange
+            var exception = new WebException();
+
+            _edubaseService.Setup(x => x.GetOrganisations())
+                           .ReturnsAsync(new List<EducationOrganisation> { new EducationOrganisation() });
+
+            _serialiser.Setup(x => x.SerialiseToJson(It.IsAny<IEnumerable<EducationOrganisation>>()))
+                       .Returns(new byte[10]);
+
+            _uploader.Setup(x => x.UploadDataToStorage(It.IsAny<byte[]>())).Throws(exception);
+
+            //Act
+            await _updater.RunUpdate();
+
+            //Assert
+            _logger.Verify(x => x.Error(exception, It.IsAny<string>()), Times.Once);
         }
     }
 }
