84efb9cb2b77d2be645674c271f4072c72a61242
Robin O'Connor <github@bytepointsoftware.com>
2017-06-09 13:26:25 +0100

    Added models and interfaces for handling educational organisation updates

---
+++ src/SFA.DAS.ReferenceData.Domain/Interfaces/Services/IEdubaseService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SFA.DAS.ReferenceData.Domain.Models.Education;
+
+namespace SFA.DAS.ReferenceData.Domain.Interfaces.Services
+{
+    public interface IEdubaseService
+    {
+        Task<IEnumerable<EducationOrganisation>> GetOrganisations();
+    }
+}
---
+++ src/SFA.DAS.ReferenceData.Domain/Models/Education/EducationOrganisation.cs
@@ -0,0 +1,7 @@
+﻿namespace SFA.DAS.ReferenceData.Domain.Models.Education
+{
+    public class EducationOrganisation
+    {
+        public string Name { get; set; }
+    }
+}
