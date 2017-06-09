94bae100c3fc8bb15e65dd1f9835bf103d4c6a0b
Robin O'Connor <github@bytepointsoftware.com>
2017-06-09 14:14:29 +0100

    Added serializer and azure storage code to updater

---
+++ src/SFA.DAS.ReferenceData.EducationOrgsImporter.WebJob/AzureStorageUploader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using Microsoft.WindowsAzure.Storage;
+using NLog;
+
+
+namespace SFA.DAS.ReferenceData.EducationOrgsImporter.WebJob
+{
+    class AzureStorageUploader
+    {
+        private readonly ILogger _logger;
+        private const string JsonContainerName = "sfa-das-reference-data";
+        private const string JsonFileName = "EducationalOrganisations.json";
+
+
+        public AzureStorageUploader(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void UploadDataToStorage(byte[] data)
+        {
+            _logger.Info($"Uploading educational organisations to Blob storage");
+
+            try
+            {
+                var storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+
+                var blobClient = storageAccount.CreateCloudBlobClient();
+                var container = blobClient.GetContainerReference(JsonContainerName);
+                container.CreateIfNotExists();
+
+                var blockBlob = container.GetBlockBlobReference(JsonFileName);
+
+                using (var stream = new MemoryStream(data))
+                {
+                    blockBlob.UploadFromStream(stream);
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e, "Error uploading Educational organisations to Blob storage");
+                throw new Exception("Error uploading Educational organisations to Blob storage", e);
+            }
+        }
+    }
+}
---
+++ src/SFA.DAS.ReferenceData.EducationOrgsImporter.WebJob/EducationalOrgainsationSerialiser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using NLog;
+using SFA.DAS.ReferenceData.Domain.Models.Education;
+
+namespace SFA.DAS.ReferenceData.EducationOrgsImporter.WebJob
+{
+    public class EducationalOrgainsationSerialiser
+    {
+        private readonly ILogger _logger;
+
+        public EducationalOrgainsationSerialiser(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public byte[] SerialiseToJson(IEnumerable<EducationOrganisation> organisations)
+        {
+            try
+            {
+                using (var ms = new MemoryStream())
+                {
+                    using (var sw = new StreamWriter(ms))
+                    {
+                        using (var jw = new JsonTextWriter(sw))
+                        {
+                            jw.Formatting = Formatting.Indented;
+
+                            var serializer = new JsonSerializer();
+                            serializer.Serialize(jw, organisations);
+                        }
+                    }
+                    return ms.ToArray();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"An error occurred generating education orgainsation json data: {ex.Message}");
+                throw new Exception($"An error occurred generating education orgainsation json data: {ex.Message}");
+            }
+        }
+    }
+}
