2b2aecf3b4209c7f78b27a92cb643fe5a1d5b2e8
Robin O'Connor <github@bytepointsoftware.com>
2017-06-09 13:04:32 +0100

    Added new web job for updating education organisations

---
+++ src/SFA.DAS.ReferenceData.EducationOrgsImporter.WebJob/Updater/EducationalOrgsUpdater.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using SFA.DAS.ReferenceData.Domain.Models;
+
+namespace SFA.DAS.ReferenceData.EducationOrgsImporter.WebJob.Updater
+{
+    public class EducationalOrgsUpdater : IEducationalOrgsUpdater
+    {
+        public EducationalOrgsUpdater()
+        {
+           
+        }
+
+        public async Task<PublicSectorOrganisationLookUp> GetData()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
