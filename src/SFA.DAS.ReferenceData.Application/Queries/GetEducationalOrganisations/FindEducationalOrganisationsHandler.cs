706b9401781fea89961721c41f79cb385bfefc3b
Robin O'Connor <github@bytepointsoftware.com>
2017-06-15 11:04:26 +0100

    Added handler code
     - To get organisations from repository

--- src/SFA.DAS.ReferenceData.Application/Queries/GetEducationalOrganisations/FindEducationalOrganisationsHandler.cs
+++ src/SFA.DAS.ReferenceData.Application/Queries/GetEducationalOrganisations/FindEducationalOrganisationsHandler.cs
@@ -1,6 +1,36 @@
-﻿namespace SFA.DAS.ReferenceData.Application.Queries.GetEducationalOrganisations
+﻿using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.ReferenceData.Api.Client.Dto;
+using SFA.DAS.ReferenceData.Domain.Interfaces.Data;
+using EducationOrganisation = SFA.DAS.ReferenceData.Domain.Models.Education.EducationOrganisation;
+
+namespace SFA.DAS.ReferenceData.Application.Queries.GetEducationalOrganisations
 {
-    public class FindEducationalOrganisationsHandler
+    public class FindEducationalOrganisationsHandler : IAsyncRequestHandler<FindEducationalOrganisationsQuery, FindEducationalOrganisationsResponse>
     {
+        private readonly IEducationalOrganisationRepository _repository;
+        
+        public FindEducationalOrganisationsHandler(IEducationalOrganisationRepository repository)
+        {
+            _repository = repository;
+        }
+      
+        public async Task<FindEducationalOrganisationsResponse> Handle(FindEducationalOrganisationsQuery query)
+        {
+            var result = await _repository.FindOrganisations(
+                query.SearchTerm,
+                query.PageSize,
+                query.PageNumber);
+
+            return new FindEducationalOrganisationsResponse
+            {
+                Organisations = new PagedApiResponse<EducationOrganisation>
+                {
+                    Data = result.Data,
+                    PageNumber = result.Page,
+                    TotalPages = result.TotalPages
+                }
+            };
+        }
     }
 }
