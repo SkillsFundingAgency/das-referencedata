aec2cf34e9138565bc76a3262fde96bebe5d7986
Michael Young <mick.young1981@googlemail.com>
2017-06-16 07:50:07 +0100

    Added missed file.

--- src/SFA.DAS.ReferenceData.Application/Queries/SearchOrganisations/SearchOrganisationsHandler.cs
+++ src/SFA.DAS.ReferenceData.Application/Queries/SearchOrganisations/SearchOrganisationsHandler.cs
@@ -10,18 +10,43 @@ namespace SFA.DAS.ReferenceData.Application.Queries.SearchOrganisations
     public class SearchOrganisationsHandler : IAsyncRequestHandler<SearchOrganisationsQuery, SearchOrganisationsResponse>
     {
         private readonly IEnumerable<IOrganisationTextSearchService> _textSearchServices;
+        private readonly IEnumerable<IOrganisationReferenceSearchService> _referenceSearchServices;
 
-        public SearchOrganisationsHandler(IEnumerable<IOrganisationTextSearchService> textSearchServices)
+        public SearchOrganisationsHandler(IEnumerable<IOrganisationTextSearchService> textSearchServices, IEnumerable<IOrganisationReferenceSearchService> referenceSearchServices)
         {
             _textSearchServices = textSearchServices;
+            _referenceSearchServices = referenceSearchServices;
         }
 
         public async Task<SearchOrganisationsResponse> Handle(SearchOrganisationsQuery query)
         {
+            var matchingReferenceSearches = _referenceSearchServices.Where(x => x.IsSearchTermAReference(query.SearchTerm));
+
+            IEnumerable<Organisation> results;
+
+            if (matchingReferenceSearches.Any())
+            {
+                results = await SearchByReference(query.SearchTerm, matchingReferenceSearches);
+            }
+            else
+            {
+                results = await SearchByText(query);
+            }
+
+            return new SearchOrganisationsResponse { Organisations = results };
+        }
+
+        private async Task<IEnumerable<Organisation>> SearchByReference(string reference, IEnumerable<IOrganisationReferenceSearchService> matchingReferenceSearches)
+        {
+            return await PerformReferenceSearch(reference, matchingReferenceSearches);
+        }
+
+        private async Task<IEnumerable<Organisation>> SearchByText(SearchOrganisationsQuery query)
+        {
             var results = await PerformTextSearch(query);
 
             var combinedResults = CombineSearchResults(results);
-            return new SearchOrganisationsResponse { Organisations = LimitResultsToMaximum(combinedResults, query.MaximumResults) };
+            return LimitResultsToMaximum(combinedResults, query.MaximumResults);
         }
 
         private IEnumerable<Organisation> LimitResultsToMaximum(IEnumerable<Organisation> combinedResults, int maximumResults)
@@ -37,6 +62,13 @@ namespace SFA.DAS.ReferenceData.Application.Queries.SearchOrganisations
         private async Task<IEnumerable<Organisation>[]> PerformTextSearch(SearchOrganisationsQuery query)
         {
             var tasks = _textSearchServices.Select(x => x.Search(query.SearchTerm, query.MaximumResults)).ToList();
+            var results = await Task.WhenAll(tasks);
+            return results;
+        }
+
+        private async Task<IEnumerable<Organisation>> PerformReferenceSearch(string reference, IEnumerable<IOrganisationReferenceSearchService> matchingReferenceSearches)
+        {
+            var tasks = matchingReferenceSearches.Select(x => x.Search(reference)).ToList();
             var results = await Task.WhenAll(tasks);
             return results;
         }
