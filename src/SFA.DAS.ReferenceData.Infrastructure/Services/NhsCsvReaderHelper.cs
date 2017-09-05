554d6b6a12e0fe85dbda6d119489729e7c2bc603
cousinst <trevor_cousins@yahoo.com>
2017-09-05 17:14:31 +0100

    AML-1590 Tidying 2

--- src/SFA.DAS.ReferenceData.Infrastructure/Services/NhsCsvReaderHelper.cs
+++ src/SFA.DAS.ReferenceData.Infrastructure/Services/NhsCsvReaderHelper.cs
@@ -48,7 +48,6 @@ namespace SFA.DAS.ReferenceData.Infrastructure.Services
                                 publicSectorOrganisationList.Add(organisation);
                             }
                         }
-
                     }
                 }
             }
