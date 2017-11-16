64d20e6728df02abf05aa7e5a08cc991f355031c
Robin O'Connor <github@bytepointsoftware.com>
2017-11-16 10:38:11 +0000

    Changed interface to support async cache operations

--- src/SFA.DAS.ReferenceData.Infrastructure/Caching/InProcessCacheProvider.cs
+++ src/SFA.DAS.ReferenceData.Infrastructure/Caching/InProcessCacheProvider.cs
@@ -1,23 +1,30 @@
 using System;
 using System.Runtime.Caching;
+using System.Threading.Tasks;
 
 namespace SFA.DAS.ReferenceData.Infrastructure.Caching
 {
     public class InProcessCacheProvider : ICacheProvider
     {
-        public T Get<T>(string key)
+        public Task<T> GetAsync<T>(string key)
         {
-            return (T)MemoryCache.Default.Get(key);
+            return Task.FromResult((T)MemoryCache.Default.Get(key));
         }
 
-        public void Set(string key, object value, TimeSpan slidingExpiration)
+        public Task SetAsync(string key, object value, TimeSpan slidingExpiration)
         {
-            MemoryCache.Default.Set(key, value, new CacheItemPolicy { SlidingExpiration = slidingExpiration });
+            return Task.Run(() =>
+            {
+                MemoryCache.Default.Set(key, value, new CacheItemPolicy {SlidingExpiration = slidingExpiration});
+            });
         }
 
-        public void Set(string key, object value, DateTimeOffset absoluteExpiration)
+        public Task SetAsync(string key, object value, DateTimeOffset absoluteExpiration)
         {
-            MemoryCache.Default.Set(key, value, new CacheItemPolicy { AbsoluteExpiration = absoluteExpiration });
+            return Task.Run(() =>
+            {
+                MemoryCache.Default.Set(key, value, new CacheItemPolicy {AbsoluteExpiration = absoluteExpiration});
+            });
         }
     }
 }
