d4ea0825b172b428a93de07cad1aab520349f6da
Craig Gumbley <craiggumbley@gmail.com>
2019-10-31 21:12:41 +0000

    Add defaults inline with table

--- src/SFA.DAS.ReferenceData.Organisations.Database/Tables/charity.sql
+++ src/SFA.DAS.ReferenceData.Organisations.Database/Tables/charity.sql
@@ -2,12 +2,12 @@
 (
 	[regno] [int] NULL,
 	[subno] [int] NULL,
-	[name] [varchar](150) NULL,
+	[name] [varchar](150) NOT NULL DEFAULT '',
 	[orgtype] [varchar](10) NULL,
 	[gd] [nvarchar](max) NULL,
 	[aob] [varchar](max) NULL,
 	[aob_defined] [int] NULL,
-	[nhs] [varchar](1) NULL,
+	[nhs] [varchar](1) NOT NULL DEFAULT '',
 	[ha_no] [int] NULL,
 	[corr] [varchar](255) NULL,
 	[add1] [varchar](35) NULL,
@@ -19,14 +19,6 @@
 	[phone] [varchar](400) NULL,
 	[fax] [int] NULL
 )
-GO
-
-ALTER TABLE [CharityData].[charity]
-ADD CONSTRAINT df_charity_0 DEFAULT '' FOR [name];
-GO
-
-ALTER TABLE [CharityData].[charity]
-ADD CONSTRAINT df_charity_1 DEFAULT '' FOR [nhs];
 GO
 
 CREATE INDEX IDX_Charity_RegNo_SubNo ON [CharityData].[charity] ([regno],[subno]) INCLUDE ([add1],[add2],[add3],[add4],[add5],[name],[orgtype],[postcode]) 
