a572da0023613696102f0230dee7b1567e3d5a1e
VasanthaKasirajan3008 <56582101+VasanthaKasirajan3008@users.noreply.github.com>
2021-10-18 14:09:21 +0100

    CON-4038

--- src/SFA.DAS.ReferenceData.Organisations.Database/Scripts/PostDeployment/PostDeployment.sql
+++ src/SFA.DAS.ReferenceData.Organisations.Database/Scripts/PostDeployment/PostDeployment.sql
@@ -10,7 +10,7 @@
 --------------------------------------------------------------------------------------
 */
 
-IF NOT EXISTS(SELECT 1 FROM sys.fulltext_indexes where object_id = 430624577)
+IF NOT EXISTS(SELECT object_id  FROM sys.fulltext_indexes WHERE EXISTS (SELECT object_id  FROM sys.fulltext_indexes  where object_id = object_id('[CharityData].[charitynamesearch]')))
 BEGIN
 	CREATE FULLTEXT INDEX ON [CharityData].charitynamesearch ([name]) KEY INDEX [PK_RegNo] ON [ftCharityCatalog] WITH CHANGE_TRACKING AUTO
 END
