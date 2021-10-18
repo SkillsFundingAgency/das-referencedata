7206a6594af65aaf0fc8dba94c7d0c65cbf8fd5a
VasanthaKasirajan3008 <56582101+VasanthaKasirajan3008@users.noreply.github.com>
2021-10-18 12:12:53 +0100

    CON-4038 depolyment issue

--- src/SFA.DAS.ReferenceData.Organisations.Database/Tables/CharityNameSearch.sql
+++ src/SFA.DAS.ReferenceData.Organisations.Database/Tables/CharityNameSearch.sql
@@ -8,5 +8,5 @@ GO
 CREATE FULLTEXT CATALOG ftCharityCatalog as DEFAULT
 GO
 
-CREATE FULLTEXT INDEX ON [CharityData].charitynamesearch ([name]) KEY INDEX [PK_RegNo] ON [ftCharityCatalog] WITH CHANGE_TRACKING AUTO
-GO
+-- CREATE FULLTEXT INDEX ON [CharityData].charitynamesearch ([name]) KEY INDEX [PK_RegNo] ON [ftCharityCatalog] WITH CHANGE_TRACKING AUTO
+-- GO
