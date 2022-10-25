2c06f6c20b416702c94f0b0dd967c459a75712e2
hasanrafique <writtentalk@hotmail.com>
2022-10-25 17:06:07 +0100

    partial check-in

--- src/SFA.DAS.ReferenceData.Infrastructure/Data/CharityImportRepository.cs
+++ src/SFA.DAS.ReferenceData.Infrastructure/Data/CharityImportRepository.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
 using FastMember;
+using Microsoft.Azure.Services.AppAuthentication;
 using SFA.DAS.ReferenceData.Domain.Interfaces.Configuration;
 using SFA.DAS.ReferenceData.Domain.Interfaces.Data;
 using SFA.DAS.ReferenceData.Domain.Models.Charity;
@@ -12,16 +14,19 @@ namespace SFA.DAS.ReferenceData.Infrastructure.Data
 {
     public class CharityImportRepository : BaseRepository, ICharityImportRepository
     {
+        private const string AzureResource = "https://database.windows.net/";
         private readonly string _connectionString;
+        private readonly string _environment;
 
         public CharityImportRepository(IConfiguration configuration) : base(configuration)
         {
             _connectionString = configuration.DatabaseConnectionString;
+            _environment = ConfigurationManager.AppSettings["EnvironmentName"];
         }
 
         public async Task ImportToStagingTable(IEnumerable<CharityImport> charityImports)
         {           
-            using (var connection = new SqlConnection(_connectionString))
+            using (var connection = OpenConnection())
             using (var bulkCopy = new SqlBulkCopy(connection))
             {
                 await connection.OpenAsync().ConfigureAwait(false);
@@ -46,6 +51,31 @@ namespace SFA.DAS.ReferenceData.Infrastructure.Data
             {
                 bulkCopy.ColumnMappings.Add(new SqlBulkCopyColumnMapping(propertyInfo.Name, propertyInfo.Name));
             }
+        }
+
+        private SqlConnection OpenConnection()
+        {
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            var environment = GetEnvironment();
+
+            return environment.Equals("LOCAL", StringComparison.CurrentCultureIgnoreCase)
+                    ? new SqlConnection(_connectionString)
+                    : new SqlConnection
+                    {
+                        ConnectionString = _connectionString,
+                        AccessToken = azureServiceTokenProvider.GetAccessTokenAsync(AzureResource).Result
+                    };
+        }
+
+        private string GetEnvironment()
+        {
+            var environment = Environment.GetEnvironmentVariable("DASENV");
+            if (string.IsNullOrEmpty(environment))
+            {
+                environment = _environment;
+            }
+
+            return environment;
         }
     }
 }
