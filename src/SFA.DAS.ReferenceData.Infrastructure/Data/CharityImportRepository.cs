702f9c689bbdd1612941c2362eb2417937e607c7
Corey <corey.faulconbridge@digital.education.gov.uk>
2022-10-31 19:51:10 +0000

    move baserepository and change MI and derived repository implementation

--- src/SFA.DAS.ReferenceData.Infrastructure/Data/CharityImportRepository.cs
+++ src/SFA.DAS.ReferenceData.Infrastructure/Data/CharityImportRepository.cs
@@ -1,47 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
 using FastMember;
-using Microsoft.Azure.Services.AppAuthentication;
+using SFA.DAS.NLog.Logger;
 using SFA.DAS.ReferenceData.Domain.Interfaces.Configuration;
 using SFA.DAS.ReferenceData.Domain.Interfaces.Data;
 using SFA.DAS.ReferenceData.Domain.Models.Charity;
-using SFA.DAS.ReferenceData.Domain.Models.Data;
 
 namespace SFA.DAS.ReferenceData.Infrastructure.Data
 {
     public class CharityImportRepository : BaseRepository, ICharityImportRepository
     {
-        private const string AzureResource = "https://database.windows.net/";
-        private readonly string _connectionString;
-        private readonly string _environment;
-
-        public CharityImportRepository(IConfiguration configuration) : base(configuration)
+        public CharityImportRepository(IConfiguration configuration, ILog logger) : base(configuration.DatabaseConnectionString, logger)
         {
-            _connectionString = configuration.DatabaseConnectionString;
-            _environment = ConfigurationManager.AppSettings["EnvironmentName"];
         }
 
         public async Task ImportToStagingTable(IEnumerable<CharityImport> charityImports)
-        {           
-            using (var connection = OpenConnection())
-            using (var bulkCopy = new SqlBulkCopy(connection))
+        {
+            await WithTransaction(async (connection, transaction) =>
             {
-                await connection.OpenAsync().ConfigureAwait(false);
-                bulkCopy.BatchSize = 1000;
-                bulkCopy.BulkCopyTimeout = 3600;
-                bulkCopy.DestinationTableName = "[CharityImport].[extract_charity_import]";
-                bulkCopy.ColumnMappings.Clear();
-                
-                PopulateBulkCopy(bulkCopy, typeof(CharityImport));
-
-                using (var reader = ObjectReader.Create(charityImports))
+                using (var bulkCopy = new SqlBulkCopy(connection))
                 {
-                    await bulkCopy.WriteToServerAsync(reader).ConfigureAwait(false);
+                    await connection.OpenAsync().ConfigureAwait(false);
+                    bulkCopy.BatchSize = 1000;
+                    bulkCopy.BulkCopyTimeout = 3600;
+                    bulkCopy.DestinationTableName = "[CharityImport].[extract_charity_import]";
+                    bulkCopy.ColumnMappings.Clear();
+
+                    PopulateBulkCopy(bulkCopy, typeof(CharityImport));
+
+                    using (var reader = ObjectReader.Create(charityImports))
+                    {
+                        await bulkCopy.WriteToServerAsync(reader).ConfigureAwait(false);
+                    }
                 }
-            }          
+
+                return;
+            });
+            
         }
 
         private void PopulateBulkCopy(SqlBulkCopy bulkCopy, Type entityType)
@@ -51,31 +48,6 @@ namespace SFA.DAS.ReferenceData.Infrastructure.Data
             {
                 bulkCopy.ColumnMappings.Add(new SqlBulkCopyColumnMapping(propertyInfo.Name, propertyInfo.Name));
             }
-        }
-
-        private SqlConnection OpenConnection()
-        {
-            var azureServiceTokenProvider = new AzureServiceTokenProvider();
-            var environment = GetEnvironment();
-
-            return environment.Equals("LOCAL", StringComparison.CurrentCultureIgnoreCase)
-                    ? new SqlConnection(_connectionString)
-                    : new SqlConnection
-                    {
-                        ConnectionString = _connectionString,
-                        AccessToken = azureServiceTokenProvider.GetAccessTokenAsync(AzureResource).Result
-                    };
-        }
-
-        private string GetEnvironment()
-        {
-            var environment = Environment.GetEnvironmentVariable("DASENV");
-            if (string.IsNullOrEmpty(environment))
-            {
-                environment = _environment;
-            }
-
-            return environment;
         }
     }
 }
